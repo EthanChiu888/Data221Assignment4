27c40b6f9660e9518fe6fc7394435298425d1ad5
Ethan.Chiu <claratseng@mac.ucalgary.ca>
2026-03-23 12:30:21 -0600

    Question 3.py Import required libraries and split the data set into 80/20.

--- Question 3.py
+++ Question 3.py
@@ -1,3 +1,26 @@
+# Q3 — Controlling Tree Complexity and Interpretability
+
+# Import required libraries
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+
+# Load dataset
+data = load_breast_cancer()
+X = data.data
+y = data.target
+feature_names = data.feature_names
+
+# Split the dataset into training and testing sets using an 80/20 split with stratification
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    stratify=y,
+    random_state=42
+)
+
+
 
 
 
